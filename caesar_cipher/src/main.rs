fb3a155faa7eaaed32bb3a8bcf067fd82d3c4d3a
Oliver Ingle <oliver.ingle@gmail.com>
2022-06-13 11:16:36 +0200

    Better caesar cipher function implemented (uses unicode)

--- caesar_cipher/src/main.rs
+++ caesar_cipher/src/main.rs
@@ -1,52 +1,53 @@
 use std::io;
+use std::char;
 
 fn main() {
 
-    println!("{}", 97 as char);
+    
 
-    // println!("Text to be ciphered:");
-    // let mut text = String::new();
-    // match io::stdin().read_line(&mut text) {
-    //     Ok(_) => println!("Success"),
-    //     Err(_) => println!("Error: failed to read input")
-    // }
+    println!("Text to be ciphered:");
+    let mut text = String::new();
+    match io::stdin().read_line(&mut text) {
+        Ok(_) => println!("Success"),
+        Err(_) => println!("Error: failed to read input")
+    }
 
-    // println!("Shift [0, 255]:");
-    // let mut shift = String::new();
-    // match io::stdin().read_line(&mut shift) {
-    //     Ok(_) => println!("Success"),
-    //     Err(_) => println!("Error: failed to read input")
-    // }
+    println!("Shift [u32 number]:");
+    let mut shift = String::new();
+    match io::stdin().read_line(&mut shift) {
+        Ok(_) => println!("Success"),
+        Err(_) => println!("Error: failed to read input")
+    }
 
-    // let text: String = text
-    //     .trim()
-    //     .to_string();
+    let text: String = text
+        .trim()
+        .to_string();
 
-    // let shift: u8 =shift
-    //     .trim()
-    //     .parse()
-    //     .expect("Error: failed to convert input to u8");
+    let shift: u32 = shift
+        .trim()
+        .parse()
+        .expect("Error: failed to convert input to u8");
 
-    // let result = cipher(&text, shift);
-    // println!("Result: {}", result)
+    let result = cipher(&text, shift);
+    println!("Result: {}", result)
+
 
 }
 
-fn cipher(text: &String, shift: u8) -> String {
+fn cipher(text: &String, shift: u32) -> String {
+    
+    let mut ciphered = String::new();
 
-    let bytes: &[u8] = text.as_bytes();
-    let mut new_bytes: Vec<u8> = vec![];
-
-    for i in 0..bytes.len() {
-        new_bytes.push(bytes[i] + shift)
-    }
-
-    match String::from_utf8(new_bytes) {
-        Ok(txt) => txt,
-        Err(_) => {
-            println!("Error: Cipher failed");
-            String::from("Cipher Failed")
+    for c in text.chars() {
+        match char::from_u32(c as u32 + shift) {
+            Some(character) => ciphered.push(character),
+            None => {
+                println!("Unable to shift character '{}', so kept as original.", c);
+                ciphered.push(c);
+            }
         }
     }
+
+    ciphered
 
 }
